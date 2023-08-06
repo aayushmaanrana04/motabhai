535562e132f6384bf8fc56f90cc6a20dc666c560
aayushmaan <aayushmaanrana04@gmail.com>
2023-08-06 13:04:49 +0530

    new

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	// server: {
-	// 	proxy: {
-	// 		'/airshot': {
-	// 			target: 'https://test.buyhatke.com/airshot',
-	// 			changeOrigin: true,
-	// 			secure: false
-	// 		}
-	// 	}
-	// }
+	plugins: [sveltekit()]
 });
